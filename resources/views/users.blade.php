7d4e5a6ccaba09052b217be8e5ee44ea573f2ed8
= <anton.graham.2011@gmail.com>
2022-08-22 20:15:49 -0400

    Update

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -6,10 +6,14 @@
 
         </div>
         
-        <h2>basic blade Template</h2>
+        <h2>Basic Blade Template</h2>
 
+        <h3>Perform logic like Add 20 + 10</h3>
         {{ 20+10 }} 
-
+        
+        
+        
+        <h3>Using if, else if and els</h3>
         @if ($user==="Anton")
         <h3>Hi {{ $user }}</h3>
         @elseif($user==='sam')
