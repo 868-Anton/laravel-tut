1c2ceb228d796eeb5a777401e7071a1b3f92aa8c
= <anton.graham.2011@gmail.com>
2022-08-24 21:52:55 -0400

    Complete InlineBlade Template

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Blade;
 
 class UsersController extends Controller
 {
     //
     function viewLoad()
-
     { 
         //dummy data
         $dummyData = ['Anton','Jack','Josh','Paul','sam'];
@@ -16,5 +16,11 @@ class UsersController extends Controller
         
         //pass data to our view file
         return view('users', ['users'=>$dummyData]);
+    }
+
+    function saySmart()
+    {
+        $cool = 'cool';
+        return Blade::render('<h1> Test </h1>',['cool'=>$cool]);
     }
 }
