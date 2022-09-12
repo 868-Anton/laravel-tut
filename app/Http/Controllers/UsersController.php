cffd3da3165131bdb3d3654eb78945f1f88d10dd
= <anton.graham.2011@gmail.com>
2022-09-12 10:38:51 -0400

    Start with DB

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+//import DB class
 use Illuminate\Support\Facades\DB;
 
 
 
 class UsersController extends Controller
 {
-    // 
+    //fetch Data from my SQL
 
     function index()
     {
-       return DB::select('select * from users');
+       return DB::select('select * from students');
     }
 }
