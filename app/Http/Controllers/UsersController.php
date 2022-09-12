4f28b7e831e39a36450f673d63e2b36a52f185f8
= <anton.graham.2011@gmail.com>
2022-09-12 11:26:44 -0400

    import user model

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,17 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 //import DB class
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+//import User mdoel
+use App\Models\User;
 
 
 class UsersController extends Controller
 {
     //fetch Data from my SQL
 
-    function index()
+    function fetchData()
     {
        return DB::select('select * from students');
     }
