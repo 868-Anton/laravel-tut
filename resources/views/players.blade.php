32b48830d80084e1e7c9323ee20369f03f30ad9e
= <anton.graham.2011@gmail.com>
2022-09-25 15:41:28 -0400

    create display of all players registered on view

---
+++ resources/views/players.blade.php
@@ -0,0 +1,27 @@
+
+<x-header/>
+
+<h1>Players List </h1>
+
+<table class="table-fixed">
+  <thead>
+    <tr>
+      <th>User Name</th>
+      <th>Online Name</th>
+      <th>Phone Number</th>
+      <th>Full Name</th>
+      <th>Division</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach ($players as $player ) 
+    <tr>
+      <td>{{ $player['Username'] }}</td>
+      <td>{{ $player['onlineName'] }}</td>
+      <td>{{ $player['phoneNumber'] }}</td>
+      <td>{{ $player['fullName'] }}</td>
+      <td>{{ $player['Divsion'] }}</td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
